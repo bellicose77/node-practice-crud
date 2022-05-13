949b70e18815a1698346033582c2ac61cc7bfb77
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-05-13 21:52:24 +0600

    post api

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+app.use(cors());
 const port = 5000;
 
 const users = [
@@ -7,7 +9,7 @@ const users = [
     {id:1,name:'Raihan',age:24,email:"Raihan@gmail.com"},
     {id:2,name:'Juwel',age:24,email:"juwel@gmail.com"},
     {id:3,name:'Asif',age:24,email:"asif@gmail.com"},
-    {id:4,name:'Rahat',age:24,email:"rahat@gmail.com"},
+    {id:4,name:'Rahat',age:24,email:"rahat@gmail.com"}
 ]
 
 app.get('/',(req,res)=>{
@@ -29,6 +31,10 @@ app.get('/users/:id',(req,res)=>{
     const id = req.params.id;
     const user = users[id];
     res.send(user);
+});
+
+app.post('/users',(req,res)=>{
+    console.log("post hitted")
 })
 
 app.listen(port,()=>{
