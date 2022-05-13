1791684ed33544c6acf8bf4a2f91ae1db77ca5f3
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-05-13 17:04:41 +0600

    dummy data

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express();
 const port = 5000;
 
 const users = [
-    
+    {id:0,name:'Dulal',age:24,email:"dulal@gmail.com"},
+    {id:1,name:'Raihan',age:24,email:"Raihan@gmail.com"},
+    {id:2,name:'Juwel',age:24,email:"juwel@gmail.com"},
+    {id:3,name:'Asif',age:24,email:"asif@gmail.com"},
+    {id:4,name:'Rahat',age:24,email:"rahat@gmail.com"},
 ]
 
 app.get('/',(req,res)=>{
